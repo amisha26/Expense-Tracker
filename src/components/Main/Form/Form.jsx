768b6e3c45f3abbc34327aa9d2a8911bde001554
amisha26 <amisha8299@gmail.com>
2021-11-03 18:36:32 +0530

    form.js

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,5 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react';
-st initialState = {
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { v4 as uuidv4 } from 'uuid';
+
+import { useSpeechContext } from '@speechly/react-client';
+import Snackbar from '../../Snackbar/Snackbar';
+import formatDate from '../../../utils/formatDate';
+import { ExpenseTrackerContext } from '../../../context/context';
+import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import useStyles from './styles';
+
+const initialState = {
   amount: '',
   category: '',
   type: 'Income',
