4af7c2d4505a248a8011d67489792347fbfd9709
amisha26 <amisha8299@gmail.com>
2021-11-03 18:34:55 +0530

    form.js

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useContext, useEffect } from 'react';
+
+const NewTransactionForm = () => {
+ 
+  const createTransaction = () => {
+  
+    
+
+  
+
+      
+
+    
+
+  return (
+    <Grid container spacing={2}>
+      <Snackbar open={open} setOpen={setOpen} />
+      
+     
+
+      
+  );
+};
+
+export default NewTransactionForm;
