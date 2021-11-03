13b63141c16ff93be1194b9539bf81291c04a7db
amisha26 <amisha8299@gmail.com>
2021-11-03 18:32:42 +0530

    context.js

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -6,7 +6,11 @@ const List = () => {
   return (
     <MUIList dense={false} className={classes.list}>
       {transactions.map((transaction) => (
-        
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            
+            
+        </Slide>
       ))}
     </MUIList>
   );
