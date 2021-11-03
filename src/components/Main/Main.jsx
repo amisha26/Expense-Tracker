6b0d6a76aac9404d1ccaefacae63233ca768e6e0
amisha26 <amisha8299@gmail.com>
2021-11-03 17:15:15 +0530

    main.js

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+const ExpenseTracker = () => {
+  
+  return (
+    <Card className={classes.root}>
+      
+      
+    
+    </Card>
+  );
+};
+
+export default ExpenseTracker;
