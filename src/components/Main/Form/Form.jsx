642387e0f318d6d5e72df93fd97bd960997f93e7
amisha26 <amisha8299@gmail.com>
2021-11-03 18:35:02 +0530

    form.js

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -15,7 +15,16 @@ const NewTransactionForm = () => {
   return (
     <Grid container spacing={2}>
       <Snackbar open={open} setOpen={setOpen} />
-      
+      <Grid item xs={12}>
+        <Typography align="center" variant="subtitle2" gutterBottom>
+        {segment ? (
+        <div className="segment">
+          {segment.words.map((w) => w.value).join(" ")}
+        </div>
+      ) : null}
+         {/* {isSpeaking ? <BigTranscript /> : 'Start adding transactions'}  */}
+        </Typography>
+      </Grid>
      
 
       
