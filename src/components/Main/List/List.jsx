9f36303478cfc647ba0402b93675b2269c2c60b1
amisha26 <amisha8299@gmail.com>
2021-11-03 18:32:33 +0530

    context.js

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+
+
+const List = () => {
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        
+      ))}
+    </MUIList>
+  );
+};
+
+export default List;
